c1090b0f3de45ba8ba7fbb156e96b233c4d6b9c7
Maria Delfina Samperi <delfisamperi@gmail.com>
2023-05-02 15:57:25 -0300

    alura org hasta el boton

---
+++ src/components/BotonForm/boton.jsx
@@ -0,0 +1,9 @@
+import "./boton.css";
+
+const Boton = () => {
+    return (
+        <button className="button">Crear</button>
+    )
+}
+
+export default Boton;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,7 @@
 import CampoTexto from "../CampoTexto/CampoTexto";
 import "./Form.css";
 import ListaOpciones from "../../ListaOpciones/ListaOpciones";
+import Boton from "../BotonForm/boton";
 
 const Form = () => {
     return (
@@ -11,6 +12,7 @@ const Form = () => {
                 <CampoTexto titulo="Puesto" placeholder="Ingresar email" />
                 <CampoTexto titulo="Foto" placeholder="Ingresar url de imagen" />
                 <ListaOpciones />
+                <Boton />    
             </form>
         </div>
     )
